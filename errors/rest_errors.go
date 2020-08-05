ff53f35b799b45c1c436373f007db327d4f99c45
sunil206 <sunil190b@gmail.com>
2020-08-05 18:17:02 -0400

    moving util functionality

---
+++ errors/rest_errors.go
@@ -0,0 +1,41 @@
+package errors
+
+import "net/http"
+
+type RestErr struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
+	Error      string `json:"error"`
+}
+
+func NewBadRequest(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusBadRequest,
+		Error:      "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusNotFound,
+		Error:      "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusInternalServerError,
+		Error:      "internal_server_error",
+	}
+}
+
+func NewAuthenticationError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusUnauthorized,
+		Error:      "un_authorized",
+	}
+}
