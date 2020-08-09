23696c98eaaffafc66e6471e9f18b3e3dad6a6bc
sunil206 <sunil190b@gmail.com>
2020-08-09 18:13:10 -0400

    Adding common logger package

---
+++ logger/logger_util.go
@@ -0,0 +1,50 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log logger
+)
+
+type logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	config := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = config.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Error(msg, tags...)
+	log.log.Sync()
+}
+
+func GetLogger() *zap.Logger {
+	return log.log
+}
